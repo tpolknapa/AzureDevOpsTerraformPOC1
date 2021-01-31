c497e103d673ad5473df0d2684ddfe75f2f9b89f
tpolk.admin <tpolk.admin@napaanesthesia.com>
2021-01-31 15:21:19 -0500

    Updated pipeline files

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 provider "azurerm" {
     version = "2.40.0"
     features {}
@@ -7,7 +6,7 @@ provider "azurerm" {
 terraform {
     backend "azurerm"{
         resource_group_name = "RG-Terraform-POC-BLOBStore"
-        storage_account_name = "tfpocstorageaccount"
+        storage_account_name = "tfpocstorageaccountp"
         container_name = "tfstate"
         key = "terraform.tfstate"
     }
@@ -39,38 +38,4 @@ resource "azurerm_container_group" "tfcg_test" {
             }
     }
 
-=======
-provider "azurerm" {
-    version = "2.40.0"
-    features {}
-
-}
-
-resource "azurerm_resource_group" "tf_test" {
-    name = "RG-Terraform-POC"
-    location = "eastus2"
-}
-
-resource "azurerm_container_group" "tfcg_test" {
-    name = "weatherapi"
-    location = azurerm_resource_group.tf_test.location
-    resource_group_name = azurerm_resource_group.tf_test.name
-
-    ip_address_type = "public"
-    dns_name_label = "TerraformPOCwa"
-    os_type = "Linux"
-
-    container {
-        name = "weatherapi"
-        image = "irietech/weatherapi"
-            cpu = "1"
-            memory = "1"
-
-            ports{
-                port = 80
-                protocol = "TCP"
-            }
-    }
-
->>>>>>> 80fc2615f9ca73b37b7b294e32309baf2e254ac0
 }
