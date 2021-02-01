ad40039c753abab928adda9063ebb7afc2d0bc3b
tpolk.admin <tpolk.admin@napaanesthesia.com>
2021-01-31 22:14:38 -0500

    Fixed azurerm version

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-provider "azurerm" {
-    version = "2.40.0"
-    features {}
-
-}
 terraform {
+    required_providers {
+      azurerm = {
+          source = "hashicorp/azurerm"
+          version = "~> 2.40.0"
+    }
+    
     backend "azurerm"{
         resource_group_name = "RG-Terraform-POC-BLOBStore"
         storage_account_name = "tfpocstorageaccountp"
